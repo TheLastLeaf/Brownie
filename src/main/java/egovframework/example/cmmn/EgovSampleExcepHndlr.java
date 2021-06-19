4ac8456146b8cb655231e4751247f03f5bd2fcd3
JH1550 <jihwn1224@naver.com>
2021-06-19 10:17:59 +0900

    Update EgovSampleExcepHndlr.java

--- src/main/java/egovframework/example/cmmn/EgovSampleExcepHndlr.java
+++ src/main/java/egovframework/example/cmmn/EgovSampleExcepHndlr.java
@@ -21,32 +21,29 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 /**
+ * @author 개발프레임웍크 실행환경 개발팀
+ * @version 1.0
  * @Class Name : EgovSampleExcepHndlr.java
  * @Description : EgovSampleExcepHndlr Class
  * @Modification Information
  * @
- * @  수정일      수정자              수정내용
+ * @ 수정일      수정자              수정내용
  * @ ---------   ---------   -------------------------------
  * @ 2009.03.16           최초생성
- *
- * @author 개발프레임웍크 실행환경 개발팀
+ * @see Copyright (C) by MOPAS All right reserved.
  * @since 2009. 03.16
- * @version 1.0
- * @see
- *
- *  Copyright (C) by MOPAS All right reserved.
  */
 public class EgovSampleExcepHndlr implements ExceptionHandler {
 
-	private static final Logger LOGGER = LoggerFactory.getLogger(EgovSampleExcepHndlr.class);
+    private static final Logger LOGGER = LoggerFactory.getLogger(EgovSampleExcepHndlr.class);
 
-	/**
-	* @param ex
-	* @param packageName
-	* @see 개발프레임웍크 실행환경 개발팀
-	*/
-	@Override
-	public void occur(Exception ex, String packageName) {
-		LOGGER.debug(" EgovServiceExceptionHandler run...............");
-	}
+    /**
+     * @param ex
+     * @param packageName
+     * @see 개발프레임웍크 실행환경 개발팀
+     */
+    @Override
+    public void occur(Exception ex, String packageName) {
+        LOGGER.debug(" EgovServiceExceptionHandler run...............");
+    }
 }
