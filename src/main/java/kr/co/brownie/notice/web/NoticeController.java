fe918f2712a7b345ce0dcf86da194cf91af1ec09
RIRINTO <ririnto@kakao.com>
2021-06-22 11:24:57 +0900

    @autowire add.

--- src/main/java/kr/co/brownie/notice/web/NoticeController.java
+++ src/main/java/kr/co/brownie/notice/web/NoticeController.java
@@ -1,8 +1,12 @@
 package kr.co.brownie.notice.web;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+
+import kr.co.brownie.notice.service.NoticeService;
 
 @Controller
 public class NoticeController {
-
+	@Autowired
+	NoticeService noticeService;
 }
--- src/main/java/kr/co/brownie/tip/web/TipController.java
+++ src/main/java/kr/co/brownie/tip/web/TipController.java
@@ -1,8 +1,12 @@
 package kr.co.brownie.tip.web;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+
+import kr.co.brownie.tip.service.TipService;
 
 @Controller
 public class TipController {
-
+	@Autowired
+	TipService tipService;
 }
