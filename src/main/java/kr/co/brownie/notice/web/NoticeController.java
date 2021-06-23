f40092367d3fdaa529aeb79c097e3be949c71df3
hyun-on <guswn9336@naver.com>
2021-06-23 15:29:09 +0900

    notice update

--- src/main/java/kr/co/brownie/notice/web/NoticeController.java
+++ src/main/java/kr/co/brownie/notice/web/NoticeController.java
@@ -5,9 +5,25 @@ import javax.annotation.Resource;
 import org.springframework.stereotype.Controller;
 
 import kr.co.brownie.notice.service.NoticeService;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class NoticeController {
 	@Resource(name = "noticeService")
 	NoticeService noticeService;
+
+	@RequestMapping(path = "/notice")
+	public String notice() {
+		return "notice/notice";
+	}
+
+	@RequestMapping(path = "/noticeAdd")
+	public String noticeAdd() {
+		return "notice/noticeAdd";
+	}
+
+	@RequestMapping(path = "/noticelist")
+	public String noticelist() {
+		return "notice/noticelist";
+	}
 }
