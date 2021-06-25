a75b8d11bf2343fc78985f29c4d094396a981a03
RIRINTO <ririnto@kakao.com>
2021-06-25 17:11:30 +0900

    CommonController.java

--- src/main/java/kr/co/brownie/common/web/CommonController.java
+++ src/main/java/kr/co/brownie/common/web/CommonController.java
@@ -3,26 +3,28 @@ package kr.co.brownie.common.web;
 import javax.annotation.Resource;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import kr.co.brownie.common.service.CommonService;
 
 @Controller
+@RequestMapping
 public class CommonController {
     @Resource(name = "commonService")
     CommonService commonService;
 
-    @RequestMapping(path = "/")
+    @GetMapping
     public String main() {
         return index();
     }
 
-    @RequestMapping(path = "/index")
+    @GetMapping("/index")
     public String index() {
         return "common/index";
     }
 
-    @RequestMapping(path = "/contact")
+    @GetMapping("/contact")
     public String contact() {
         return "common/contact";
     }
