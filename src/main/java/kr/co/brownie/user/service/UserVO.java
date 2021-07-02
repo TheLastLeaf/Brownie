ffcf8d0f9b47335c5b7d2f9f3e8f692783824357
hyun-on <guswn9336@naver.com>
2021-07-02 14:55:07 +0900

    세웅님의 VO

--- src/main/java/kr/co/brownie/user/service/UserVO.java
+++ src/main/java/kr/co/brownie/user/service/UserVO.java
@@ -1,7 +1,10 @@
 package kr.co.brownie.user.service;
 
+import lombok.Data;
+
 import java.util.Date;
 
+@Data
 public class UserVO {
     private String userId;
     private String lolId;
@@ -13,106 +16,4 @@ public class UserVO {
     private String inUserId;
     private String upUserId;
 
-    public UserVO(){
-
-    }
-
-    public UserVO(String userId, String lolId, String nickName, int blackStack, String status, Date inDate, Date modDate, String inUserId, String upUserId) {
-        this.userId = userId;
-        this.lolId = lolId;
-        this.nickName = nickName;
-        this.blackStack = blackStack;
-        this.status = status;
-        this.inDate = inDate;
-        this.modDate = modDate;
-        this.inUserId = inUserId;
-        this.upUserId = upUserId;
-    }
-
-    public String getUserId() {
-        return userId;
-    }
-
-    public void setUserId(String userId) {
-        this.userId = userId;
-    }
-
-    public String getLolId() {
-        return lolId;
-    }
-
-    public void setLolId(String lolId) {
-        this.lolId = lolId;
-    }
-
-    public String getNickName() {
-        return nickName;
-    }
-
-    public void setNickName(String nickName) {
-        this.nickName = nickName;
-    }
-
-    public int getBlackStack() {
-        return blackStack;
-    }
-
-    public void setBlackStack(int blackStack) {
-        this.blackStack = blackStack;
-    }
-
-    public String getStatus() {
-        return status;
-    }
-
-    public void setStatus(String status) {
-        this.status = status;
-    }
-
-    public Date getInDate() {
-        return inDate;
-    }
-
-    public void setInDate(Date inDate) {
-        this.inDate = inDate;
-    }
-
-    public Date getModDate() {
-        return modDate;
-    }
-
-    public void setModDate(Date modDate) {
-        this.modDate = modDate;
-    }
-
-    public String getInUserId() {
-        return inUserId;
-    }
-
-    public void setInUserId(String inUserId) {
-        this.inUserId = inUserId;
-    }
-
-    public String getUpUserId() {
-        return upUserId;
-    }
-
-    public void setUpUserId(String upUserId) {
-        this.upUserId = upUserId;
-    }
-
-    @Override
-    public String toString() {
-        return "UserVO{" +
-                "userId='" + userId + '\'' +
-                ", lolId='" + lolId + '\'' +
-                ", nickName='" + nickName + '\'' +
-                ", blackStack=" + blackStack +
-                ", status='" + status + '\'' +
-                ", inDate=" + inDate +
-                ", modDate=" + modDate +
-                ", inUserId='" + inUserId + '\'' +
-                ", upUserId='" + upUserId + '\'' +
-                '}';
-    }
 }
