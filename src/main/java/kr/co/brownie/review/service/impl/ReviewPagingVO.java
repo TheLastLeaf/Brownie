4bf12768f637d4ac1809126b551b12d720668fda
ParkSeWoong95 <atpdnd@gmail.com>
2021-07-08 16:49:52 +0900

    박세웅입니다. review_sql 에서 삽질좀 했습니다.

--- src/main/java/kr/co/brownie/review/service/impl/ReviewPagingVO.java
+++ src/main/java/kr/co/brownie/review/service/impl/ReviewPagingVO.java
@@ -1,5 +1,78 @@
 package kr.co.brownie.review.service.impl;
 
+import lombok.Data;
+
+@Data
 public class ReviewPagingVO {
+	// 현재 페이지 번호
+	private int num = 1;
+	// 게시물 총 갯수
+	private int totalCount;
+	// 한 페이지에 출력할 게시물 갯수
+	private int postNum = 9;
+	// 하단 페이징 번호([게시물 총 갯수 ÷ 한페이지에 출력할 갯수의]의 올림)
+	private int pageNum;
+	// 출력할 시작 게시물
+	private int startPost;
+	// 출력할 마지막 게시물
+	private int endPost;
+	// 한번에 표시할 페이징 번호의 갯수
+	private int pageNumCnt = 5;
+	// 표시되는 페이지 번호 중 마지막 번호
+	private int endPageNum;
+	// 표시되는 페이지 번호 중 첫번째 번호
+	private int startPageNum;
+	// 다음/이전 표시여부
+	private boolean prev;
+	private boolean next;
+
+	public void setTotalCount(int totalCount) {
+		this.totalCount = totalCount;
+		dataCalc();
+	}
+
+	private void dataCalc() {
+		// 마지막 번호
+		endPageNum = (int) (Math.ceil((double) num / (double) pageNumCnt) * pageNumCnt);
+		// 시작 번호
+		startPageNum = endPageNum - (pageNumCnt - 1);
+		// 마지막 번호 재계산 (예: 총 13페이지를 5로 나누면 3이 나와야하는데 15페이지까지 나오기 떄문에)
+		int endPageNum_tmp = (int) (Math.ceil((double) totalCount / (double) postNum));
+		if (endPageNum > endPageNum_tmp) {
+			endPageNum = endPageNum_tmp;
+		}
+		prev = startPageNum == 1 ? false : true;
+		next = endPageNum * postNum >= totalCount ? false : true;
+
+		startPost = (num - 1) * postNum + 1;
+		endPost = startPost + (postNum - 1);
+	}
+
+	public String getSearchTypeKeyword() {
+		if (searchType.equals("") || keyword.equals("")) {
+			return "";
+		} else {
+			return "&searchType=" + searchType + "&keyword=" + keyword;
+		}
+	}
+
+	private String searchType = "title";
+	private String keyword = "";
+
+	public String getSearchType() {
+		return searchType;
+	}
+
+	public void setSearchType(String searchType) {
+		this.searchType = searchType;
+	}
+
+	public String getKeyword() {
+		return keyword;
+	}
+
+	public void setKeyword(String keyword) {
+		this.keyword = keyword;
+	}
 
 }
