4fe2f66a8b570392a1ee31f6cfb2fdd5d8533b57
RIRINTO <ririnto@kakao.com>
2021-07-08 14:55:53 +0900

    저장위치 변경
    CommonController.java
    sample.java
    servlet-context.xml

--- src/main/java/kr/co/brownie/sample.java
+++ src/main/java/kr/co/brownie/sample.java
@@ -1,16 +1,6 @@
 package kr.co.brownie;
 
-import com.google.gson.JsonObject;
-import org.apache.commons.io.FileUtils;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.web.multipart.MultipartFile;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletRequestWrapper;
-import java.io.File;
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.UUID;
+import org.springframework.web.bind.annotation.GetMapping;
 
 public class sample {
     @GetMapping(path = "/sample")
