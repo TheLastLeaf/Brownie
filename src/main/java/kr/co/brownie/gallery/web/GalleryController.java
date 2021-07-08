41185d459451a2a838ebf7630ac8b2bd7aeafbf9
JH1550 <jihwn1224@naver.com>
2021-07-08 12:39:03 +0900

    오류commit
    오류commit

--- src/main/java/kr/co/brownie/gallery/web/GalleryController.java
+++ src/main/java/kr/co/brownie/gallery/web/GalleryController.java
@@ -18,6 +18,19 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.UUID;
+
+import com.google.gson.JsonObject;
+import org.apache.commons.io.FileUtils;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletRequestWrapper;
+
 @Controller
 @RequestMapping("/gallery")
 public class GalleryController {
@@ -69,13 +82,48 @@ public class GalleryController {
 		
 		model.addAttribute("galleryVO", galleryVO);
 		
-		return "gallery/detail";
+		return "gallery/galleryDetail";
 	}
 
 	@GetMapping("/add")
 	public String details_add_gallery() {
-		return "gallery/galleryDetail";
+		return "gallery/galleryAdd";
 	}
+	
+	@RequestMapping(value = "/uploadSummernoteImageFile", produces = "application/json; charset=utf8")
+    @ResponseBody
+    public String uploadSummernoteImageFile(@RequestParam("file") MultipartFile multipartFile, HttpServletRequest request) {
+		System.out.println(1);
+        JsonObject jsonObject = new JsonObject();
+        System.out.println(2);
+
+        
+        String fileRoot = "C:\\summernote_image\\"; // 외부경로로 저장을 희망할때.
+        
+
+        // 내부경로로 저장
+        System.out.println(3);
+        String originalFileName = multipartFile.getOriginalFilename();    //오리지날 파일명
+        assert originalFileName != null;
+        String extension = originalFileName.substring(originalFileName.lastIndexOf("."));    //파일 확장자
+        String savedFileName = UUID.randomUUID() + extension;    //저장될 파일 명
+        System.out.println(4);
+        File targetFile = new File(fileRoot + savedFileName);
+        try {
+            InputStream fileStream = multipartFile.getInputStream();
+            FileUtils.copyInputStreamToFile(fileStream, targetFile);    //파일 저장
+            jsonObject.addProperty("url", "/summernoteImage/" + savedFileName); // contextroot + resources + 저장할 내부 폴더명
+            jsonObject.addProperty("responseCode", "success");
+            System.out.println(5);
+        } catch (IOException e) {
+            FileUtils.deleteQuietly(targetFile);    //저장된 파일 삭제
+            jsonObject.addProperty("responseCode", "error");
+            e.printStackTrace();
+            System.out.println(6);
+        }
+        System.out.println(5);
+        return jsonObject.toString();
+    }
 
 	@GetMapping("/update")
 	public String details_modify_gallery() {
